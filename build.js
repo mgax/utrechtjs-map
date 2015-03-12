3a3e66fd0520f0b132faba8405a172f8526920f5
Alex Morega <alex@grep.ro>
2015-03-12 18:07:39 +0100

    convert to geojson, write to disk

--- build.js
+++ build.js
@@ -1,4 +1,6 @@
+var fs = require('fs');
 var request = require('request');
+var osmtogeojson = require('osmtogeojson');
 
 var pub_query = '[out:json][timeout:25];node[amenity~"pub|cafe"](52.0718,5.0938,52.0999,5.1570);out;';
 
@@ -6,11 +8,11 @@ var pub_query = '[out:json][timeout:25];node[amenity~"pub|cafe"](52.0718,5.0938,
 function overpass(q, cb) {
   var url = 'http://overpass-api.de/api/interpreter?data=' + encodeURIComponent(q);
   request(url, function(err, res, body) {
-    cb(JSON.parse(body));
+    cb(osmtogeojson(JSON.parse(body)));
   });
 }
 
 
 overpass(pub_query, function(pubs) {
-  console.log(pubs);
+  fs.writeFileSync('data.json', JSON.stringify(pubs, null, 2));
 });
